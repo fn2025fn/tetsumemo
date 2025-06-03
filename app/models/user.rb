1fbd59744e610e97bf80f188a9f992db0ff51e51
fn2025fn <usui.fn.2024@gmail.com>
2025-06-03 16:58:43 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :children
+
   validates :name, presence: true
 end

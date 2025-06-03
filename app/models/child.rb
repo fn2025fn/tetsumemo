6ffe4b265b436c52bf144aa5beb4e1646c19b637
fn2025fn <usui.fn.2024@gmail.com>
2025-06-03 16:43:36 +0900

    Childモデル・マイグレーション作成

---
+++ app/models/child.rb
@@ -0,0 +1,4 @@
+class Child < ApplicationRecord
+  belongs_to :user
+  has_one_attached :icon
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_03_000540) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_03_061927) do
+  create_table "children", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_children_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,4 +33,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_03_000540) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "children", "users"
 end

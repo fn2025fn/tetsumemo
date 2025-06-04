2832f2c72cbb221ea02e86230235e4d9dbd67e94
fn2025fn <usui.fn.2024@gmail.com>
2025-06-04 16:51:24 +0900

    編集削除機能実装

--- app/controllers/task_templates_controller.rb
+++ app/controllers/task_templates_controller.rb
@@ -1,10 +1,12 @@
 class TaskTemplatesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_task_template, only: [:show, :edit, :update, :destroy]
   def index
     @task_templates = TaskTemplate.all
   end
 
   def new
-    @task_templates = TaskTemplate.all
+    @task_template = TaskTemplate.new
   end
 
   def create
@@ -14,6 +16,31 @@ class TaskTemplatesController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task_template.update(task_template_params)
+      redirect_to task_templates_path, notice: 'テンプレートを更新しました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task_template.destroy
+    redirect_to task_templates_path, notice: 'テンプレートを削除しました'
+  end
+
+  private
+
+  def set_task_template
+    @task_template = TaskTemplate.find(params[:id])
   end
 
   def task_template_params

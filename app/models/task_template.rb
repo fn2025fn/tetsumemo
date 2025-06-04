07bf38a051930565c407c580f12befc3d8079901
fn2025fn <usui.fn.2024@gmail.com>
2025-06-04 14:34:36 +0900

    モデルの作成

--- app/models/task_template.rb
+++ app/models/task_template.rb
@@ -1,2 +1,6 @@
 class TaskTemplate < ApplicationRecord
+  has_many :task_logs, dependent: :destroy
+
+  validates :title, presence: true
+  validates :point, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end

0337a7638b3f39e6f69d6d1b36554175e536111e
fn2025fn <usui.fn.2024@gmail.com>
2025-06-05 09:20:28 +0900

    バリデーション設定

--- app/models/task_log.rb
+++ app/models/task_log.rb
@@ -2,4 +2,7 @@ class TaskLog < ApplicationRecord
   belongs_to :child
   belongs_to :task_template
   has_one_attached :image
+
+  validates :child_id, presence: true
+  validates :task_template_id, presence: true
 end

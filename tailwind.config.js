1a08e77d7c6948599cba31627b3fcacf7c83e707
fn2025fn <usui.fn.2024@gmail.com>
2025-06-09 14:28:37 +0900

    通常CSSに変更、サインアップ・ログイン画面ビュー作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ module.exports = {
   content: [
     "./app/views/**/*.{html,erb}",
     "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js"
+    "./app/javascript/**/*.js",
+    "./app/assets/stylesheets/**/*.{css,scss}"
   ],
   theme: {
     extend: {},

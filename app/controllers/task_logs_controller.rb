520921b35e2bda3cceaae8d91c7a7c7323594268
fn2025fn <usui.fn.2024@gmail.com>
2025-06-30 17:10:45 +0900

    TaskLogsControllerのリファクタリング

--- app/controllers/task_logs_controller.rb
+++ app/controllers/task_logs_controller.rb
@@ -1,8 +1,9 @@
 class TaskLogsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_child
+  before_action :set_task_log, only: [:show, :edit, :update, :destroy]
 
   def new
-    @child = Child.find(params[:child_id])
     @task_log = TaskLog.new
     @task_templates = TaskTemplate.all
   end
@@ -10,60 +11,54 @@ class TaskLogsController < ApplicationController
   def create
     @task_log = TaskLog.new(task_log_params)
     @task_log.from = params[:task_log][:from]
+    @child = Child.find(@task_log.child_id)
+
     if @task_log.save
-      if @task_log.from != 'memory'
-        @child = Child.find(@task_log.child_id)
-        redirect_to child_path(@child), notice: 'お手伝いを記録し、ごほうびポイント加算'
+      if @task_log.from == 'memory'
+        redirect_to album_child_task_logs_path(@child), notice: '思い出を登録しました'
       else
-        redirect_to album_child_task_logs_path(@task_log.child_id), notice: '思い出を登録しました'
+        redirect_to child_path(@child), notice: 'お手伝いを記録し、ごほうびポイント加算'
       end
     else
-      @child = Child.find(task_log_params[:child_id])
       @task_templates = TaskTemplate.all
-      if params[:task_log][:from] == 'memory'
-        render :new_memory
-      else
-        render :new
-      end
+      render @task_log.from == 'memory' ? :new_memory : :new
     end
   end
 
   def album
-    @child = Child.find(params[:child_id])
     @image_logs = @child.task_logs.with_attached_image.select { |log| log.image.attached? }
   end
 
   def show
-    @child = Child.find(params[:child_id])
-    @task_log = TaskLog.find(params[:id])
   end
 
   def edit
-    @child = Child.find(params[:child_id])
-    @task_log = TaskLog.find(params[:id])
   end
 
   def update
-    @child = Child.find(params[:child_id])
-    @task_log = TaskLog.find(params[:id])
     @task_log.update(task_log_params)
     redirect_to album_child_task_logs_path(@child)
   end
 
   def destroy
-    @child = Child.find(params[:child_id])
-    @task_log = TaskLog.find(params[:id])
     @task_log.destroy
     redirect_to child_path(@child), notice: 'お手伝い記録を削除しました'
   end
 
   def new_memory
-    @child = Child.find(params[:child_id])
     @task_log = TaskLog.new
     @task_templates = TaskTemplate.all
   end
 
   private
+
+  def set_child
+    @child = Child.find(params[:child_id])
+  end
+
+  def set_task_log
+    @task_log = TaskLog.find(params[:id])
+  end
 
   def task_log_params
     params.require(:task_log).permit(:child_id, :task_template_id, :image, :title,
